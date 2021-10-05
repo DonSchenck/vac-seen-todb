985fb861e8cc6fef0128919cea85bb6cf57a4780
Don Schenck <don.schenck@gmail.com>
2021-10-05 17:14:05 -0400

    creation

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace vac_seen_todb
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
