579bc828ed72a9df8efbea7d96a92f3818b6d13b
Don Schenck <don.schenck@gmail.com>
2022-07-15 14:44:54 -0400

    Work around ID field

--- Program.cs
+++ Program.cs
@@ -67,7 +67,7 @@ namespace vac_seen_todb
                             vaxcount++;
                         }
                     }
-                    consumer.Close();
+//                    consumer.Close();
                }
             }
             catch (Exception e)
