b81bfad3c49bedfa27ddf85bfafaf3c740cc44d6
Don Schenck <don.schenck@gmail.com>
2022-07-15 14:40:16 -0400

    Work around ID field

--- VaccinationEvent.cs
+++ VaccinationEvent.cs
@@ -4,6 +4,7 @@ namespace vac_seen_todb
 {
    public class VaccinationEvent
     {
+        public Guid Id;
         public String RecipientID;
         public DateTime EventTimestamp;
         public string CountryCode;
